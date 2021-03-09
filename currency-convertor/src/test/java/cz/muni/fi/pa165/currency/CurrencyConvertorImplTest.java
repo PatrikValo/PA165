fc66ac167c4a41e851e2856aa961150e7b4852ef
Patrik Valo <xvalo2@fi.muni.cz>
2021-03-09 11:34:25 +0100

    Adding testConvert test

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,14 +1,29 @@
 package cz.muni.fi.pa165.currency;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CurrencyConvertorImplTest {
 
     @Test
     public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+        var eur = Currency.getInstance("EUR");
+        var czk = Currency.getInstance("CZK");
+        var exchangeRate = new BigDecimal("25.21");
+
+        var mock = createMock(eur, czk, exchangeRate);
+        var convertor = new CurrencyConvertorImpl(mock);
+
+        var amount = new BigDecimal("100");
+        assertThat(convertor.convert(eur, czk, amount))
+                .isEqualTo(amount.multiply(exchangeRate));
     }
 
     @Test
@@ -36,4 +51,13 @@ public class CurrencyConvertorImplTest {
         fail("Test is not implemented yet.");
     }
 
+    public ExchangeRateTable createMock(Currency source, Currency target, BigDecimal result) {
+        try {
+            var exchange = mock(ExchangeRateTable.class);
+            when(exchange.getExchangeRate(source, target)).thenReturn(result);
+            return exchange;
+        } catch (ExternalServiceFailureException e) {
+            throw new RuntimeException("Mock configuration failed", e);
+        }
+    }
 }
