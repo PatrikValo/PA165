4f704f37a347de31f8ffa48d13b06685139033f1
Patrik Valo <xvalo2@fi.muni.cz>
2021-03-09 11:48:06 +0100

    Adding null test cases

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -12,10 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CurrencyConvertorImplTest {
 
+    private final Currency eur = Currency.getInstance("EUR");
+    private final Currency czk = Currency.getInstance("CZK");
+
     @Test
     public void testConvert() {
-        var eur = Currency.getInstance("EUR");
-        var czk = Currency.getInstance("CZK");
         var exchangeRate = new BigDecimal("25.21");
 
         var mock = createMock(eur, czk, exchangeRate);
@@ -28,12 +30,26 @@ public class CurrencyConvertorImplTest {
 
     @Test
     public void testConvertWithNullSourceCurrency() {
-        fail("Test is not implemented yet.");
+        var exchangeRate = new BigDecimal("27.22");
+
+        var mock = createMock(null, czk, exchangeRate);
+        var convertor = new CurrencyConvertorImpl(mock);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> convertor.convert(null, czk, new BigDecimal("200")))
+                .withMessage("One of the argument is null.");
     }
 
     @Test
     public void testConvertWithNullTargetCurrency() {
-        fail("Test is not implemented yet.");
+        var exchangeRate = new BigDecimal("26.22");
+
+        var mock = createMock(eur, null, exchangeRate);
+        var convertor = new CurrencyConvertorImpl(mock);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> convertor.convert(eur, null, new BigDecimal("200")))
+                .withMessage("One of the argument is null.");
     }
 
     @Test
@@ -55,6 +71,10 @@ public class CurrencyConvertorImplTest {
         try {
             var exchange = mock(ExchangeRateTable.class);
             when(exchange.getExchangeRate(source, target)).thenReturn(result);
+            if (source == null || target == null) {
+                when(exchange.getExchangeRate(source, target))
+                        .thenThrow(new IllegalArgumentException("One of the argument is null."));
+            }
             return exchange;
         } catch (ExternalServiceFailureException e) {
             throw new RuntimeException("Mock configuration failed", e);
